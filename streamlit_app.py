970c4d2447889662b52487686814c6a67a0bd3ee
E-esthy1981 <bertilleekee@gmail.com>
2025-02-14 12:27:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,15 +14,21 @@ def main():
     st.write(f"Vous avez choisi : {option}")
 
     # Slider
-    value = st.slider("Sélectionnez la longueur des petals", 0, 100, 50)
+    value = st.slider("Sélectionnez la longueur des Pétals", 0, 100, 50)
     st.write(f"Valeur sélectionnée : {value}")
     
      # Slider
-  #  value = st.slider("Sélectionnez une valeur", 0, 100, 50)
+    value = st.slider("Sélectionnez la largeur des Pétals", 0, 100, 50)
+    #st.write(f"Valeur sélectionnée : {value}")
+    
+ # Slider
+    value = st.slider("Sélectionnez la longueur des Sépals", 0, 100, 50)
     #st.write(f"Valeur sélectionnée : {value}")
 
-    # Affichage d'une image
-   # st.image("https://picsum.photos/200/300", caption="Image aléatoire")
+ # Slider
+    value = st.slider("Sélectionnez la largeur des Sépals", 0, 100, 50)
+    #st.write(f"Valeur sélectionnée : {value}")
+
 
 if __name__ == "__main__":
     main()
