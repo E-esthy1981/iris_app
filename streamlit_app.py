d9bc95ca7c9449d021e25a02cec72d8a6286b526
E-esthy1981 <bertilleekee@gmail.com>
2025-02-16 16:01:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,24 +9,19 @@ def main():
     if st.button("Soumettre"):
         st.write(f"Bonjour, {user_input}!")
 
+    # Sélection dans une liste déroulante
+    option = st.selectbox("Choisissez une option :", ["Option 1", "Option 2", "Option 3"])
+    st.write(f"Vous avez choisi : {option}")
+
     # Slider
-    value = st.slider("Sélectionnez la longueur des Pétals", 0, 100, 50)
-    st.write(f"Valeur sélectionnée : {value}")
-    
-     # Slider
-    value = st.slider("Sélectionnez la largeur des Pétals", 0, 100, 50)
-    st.write(f"Valeur sélectionnée : {value}")
-    
- # Slider
-    value = st.slider("Sélectionnez la longueur des Sépals", 0, 100, 50)
+    value = st.slider("Sélectionnez une valeur", 0, 100, 50)
     st.write(f"Valeur sélectionnée : {value}")
 
- # Slider
-    value = st.slider("Sélectionnez la largeur des Sépals", 0, 100, 50)
-    st.write(f"Valeur sélectionnée  : {value}")
+    # Affichage d'une image
+    st.image("https://picsum.photos/200/300", caption="Image aléatoire")
 
-   if __name__ == "__main__":
-    main() 
+if __name__ == "__main__":
+    main()
        
 st.write("bonjour le monde")
 
